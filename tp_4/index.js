64d03f4434dbfef7bc432470b3ed5b1378dd286c
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-11 18:45:13 +0100

    static weather resource created

--- tp_4/index.js
+++ tp_4/index.js
@@ -3,6 +3,41 @@ import express from "express";
 const PORT = 8000;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello word"));
+app.set("json spaces", 2);
+app.get("/", (req, res) => res.json({ msg: "hello word" }));
+
+app.get("/weatherinfo", (req, res) => {
+  res.json({
+    weatherinformation: [
+      {
+        willaya: "bechar",
+        temprature: 60,
+        humidty: 7,
+        position: {
+          latitude: 1.04231313414,
+          langitude: -0.842996368726387
+        }
+      },
+      {
+        willaya: "bechar",
+        temprature: 60,
+        humidty: 7,
+        position: {
+          latitude: 1.04231313414,
+          langitude: -0.842996368726387
+        }
+      },
+      {
+        willaya: "bechar",
+        temprature: 60,
+        humidty: 7,
+        position: {
+          latitude: 1.04231313414,
+          langitude: -0.842996368726387
+        }
+      }
+    ]
+  });
+});
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
