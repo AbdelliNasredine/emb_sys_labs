e6370c71f7769066892de9288b3ae2ba3f8dcbd5
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-14 02:07:29 +0100

    added cities.js + position (longitude,altitude) to weather station information

---
+++ tp_4/libs/cities.js
@@ -0,0 +1,19 @@
+// module.exports = [
+//   'Alger',
+//   'Oran',
+//   'Constantite',
+//   'Annaba',
+//   'Blida',
+//   'Batna',
+//   'Djelfa',
+//   'Setif',
+//   'Sidi bel Abbes',
+//   'Tlemcen',
+//   'Ouargla',
+//   'Bechar',
+//   'Tindouf',
+//   'Tamanrasset',
+//   'Adrar'
+// ];
+
+module.exports = [{ name: "Alger", lat: 36.753889, lon: 3.058889 }];
--- tp_4/routes/weather.js
+++ tp_4/routes/weather.js
@@ -1,23 +1,35 @@
 module.exports = app => {
   // weather model (weather_info table)
   const Weather = app.db.models.Weather;
+  const cities = require("../libs/cities.js");
 
   // /show is used to see the sqlite
   // database(weather info table)
   app.get("/show", (req, res) => {
     Weather.findAll({}).then(winfo => {
-      res.json({ weatherinformation: winfo });
+      const willaya = cities[0];
+      const position = {
+        latitude: willaya.lat,
+        longitude: willaya.lon
+      };
+      const weatherinfo = winfo.map(station => {
+        return {
+          station,
+          position
+        }
+      });
+      res.json({ weatherinfo, position });
     });
   });
 
-  // /gen/:number route is used to gerenate 
+  // /gen/:number route is used to gerenate
   // random weather information
   app.get("/gen/:number", (req, res) => {
     let number = req.param("number");
     console.log(number);
     while (number != 0) {
       Weather.create({
-        willaya: "Bechar",
+        willaya: cities[0].name,
         temp: Math.floor(Math.random() * 60),
         wind: Math.floor(Math.random() * 100)
       }).then(winfo => {
