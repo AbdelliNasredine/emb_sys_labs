1806c57fcf8470868a6150e87619e18dd55b0220
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-18 02:50:23 +0100

    added willaya routes

--- tp_4/servers/rpi_server/libs/cities.js
+++ tp_4/servers/rpi_server/libs/cities.js
@@ -1,19 +1,19 @@
-// module.exports = [
-//   'Alger',
-//   'Oran',
-//   'Constantite',
-//   'Annaba',
-//   'Blida',
-//   'Batna',
-//   'Djelfa',
-//   'Setif',
-//   'Sidi bel Abbes',
-//   'Tlemcen',
-//   'Ouargla',
-//   'Bechar',
-//   'Tindouf',
-//   'Tamanrasset',
-//   'Adrar'
-// ];
-
-module.exports = [{ name: "Alger", lat: 36.753889, lon: 3.058889 }];
+module.exports = [
+  { code: 1, name: "Adrar", lat: 28.1918767, lon: -0.2642498 },
+  { code: 2, name: "Chlef", lat: 36.1370738, lon: 1.3147095 },
+  { code: 31, name: "Oran", lat: 35.6921892, lon: -0.6298704 },
+  { code: 19, name: "Sétif", lat: 36.1814245, lon: 5.4016232 },
+  { code: 16, name: "Alger", lat: 36.753889, lon: 3.058889 },
+  { code: 25, name: "Constantine", lat: 36.3496828, lon: 6.6331647 },
+  { code: 23, name: "Annaba", lat: 36.9139263, lon: 7.7647559 },
+  { code: 7, name: "Biskra", lat: 34.8490149, lon: 5.7322847 },
+  { code: 17, name: "Djelfa", lat: 34.6594603, lon: 3.2054293 },
+  { code: 32, name: "El Bayadh", lat: 33.6686249, lon: 1.0301362 },
+  { code: 8, name: "Bechar", lat: 31.624429, lon: -2.2218167 },
+  { code: 30, name: "Ouargla", lat: 31.9419508, lon: 5.3367763 },
+  { code: 11, name: "Tamanrasset", lat: 22.8056012, lon: 5.512557 },
+  { code: 37, name: "Tindouf", lat: 27.6631409, lon: -8.1434482 },
+  { code: 14, name: "Tiaret", lat: 35.3613215, lon: 1.3157793 },
+  { code: 20, name: "Saida", lat: 34.8490151, lon: 0.1402423 },
+  { code: 6, name: "Béjaia", lat: 36.7380443, lon: 5.0511304 }
+]; 
---
+++ tp_4/servers/rpi_server/routes/willaya.js
@@ -0,0 +1,14 @@
+module.exports = app => {
+  const willayas = require("../libs/cities");
+
+  // get al the willayas list with thier geo cords
+  app.get("/wilaya/all", (req, res) => res.json({ status: "succes", data: willayas}));
+
+  // get a specific willaya by code 
+  app.get("/wilaya/:code", (req, res) => {
+    const code = req.param("code");
+    const willaya = willayas.filter((w) => w.code == code);
+    if(willaya.length) return res.json({ status: "succes", data: willayas});
+    return res.json({ status: "Error", msg: `Couldn't find willaya with code = ${code}`});
+  });
+}
