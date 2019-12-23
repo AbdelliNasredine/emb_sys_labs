679393c4735c24b22d51b1891e3c2dbcdf60102c
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-23 04:36:53 +0100

    fixed merge conflict

--- tp_4/servers/rpi_server/libs/random.js
+++ tp_4/servers/rpi_server/libs/random.js
@@ -4,5 +4,7 @@ const _r = (nb) =>  Math.floor(Math.random() * nb);
 module.exports = {
   willaya: () => cities[_r(cities.length)].name,
   temperature: () => _r(60),
-  wind: () => _r(100)
+  wind: () => _r(100),
+  humidity: () => _r(1),
+  humidity: () => _r(1),
 }
